23a6db40df3199426e6b7fc30e71881d9514763a
Ray Ashir <rayeed.ashir@gmail.com>
2021-09-12 20:28:29 -0700

    added post to category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -23,20 +23,26 @@ router.get('/:id', async (req, res) => {
       const categoryData = await Category.findByPk(req.params.id, {
         include: [{ model: Product }],
       });
-  
       if (!categoryData) {
         res.status(404).json({ message: 'No category found with that id!' });
         return;
       }
-  
       res.status(200).json(categoryData);
     } catch (err) {
       res.status(500).json(err);
     }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
